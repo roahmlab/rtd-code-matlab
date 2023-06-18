b7630c5fd06e6e8d6169e9f3b787f2f23eb3e6ae
Sukruthi Chidananda <123084588+Sukruthi-C@users.noreply.github.com>
2023-06-18 01:24:18 -0400

    Update FRS_Generator_speed_change.m

--- FRS_Generator_speed_change.m
+++ FRS_Generator_speed_change.m
@@ -1,72 +1,89 @@
 
-classdef FRS_Generator_speed_change
+classdef FRS_Generator_speed_change  < rtd.planner.reachsets.ReachSetGenerator
   
     properties
-        state %consists of x & y x(t) & y(t)
-        head %heading of the vehicle h(t)
-        yaw_rate %r rad/sec
-        u0 %initial logitudnal speed
-        v0 %initial lateral speed
-        r0 %initial yaw rate
+        robotState %states of the robot & UUID
     end
      
     properties
-        cache_max_size = 100; % Set the desired cache maximum size
-        speed_lat %lateral speed v(t)
-        speed_log %logitudnal speed u(t) 
-        p_u %slice parameter for speed
-        t %time
-        %p_y %slice parameter for lane or direction change
+        cache_max_size = 100 % Set the desired cache maximum size
+        z_matrix
+        vehrs
+        constraints
+        generate_frs
+        reach_sets_from_file
     end
 
     
     methods
+        function self = FRS_Generator_speed_change()
+            self.reach_sets_from_file = self.generate_ReachableSet('converted_Au_frs.h5');
+            disp('Done with FRS generation.');
+        end
+    end
+
+    methods(Access=protected)
       
-        function reach = generateReachableSet(file) %probably put this inside the constructor
-            reachsets = loaders.RSLoader('converted_Au_frs.h5', do_caching=true, preload_data=false, preload_meta=false);
-            downstream_group = reachsets.getGroup(0);
-            arr_of_further_downstream_groups = reachsets.getGroup(0).getGroup();
+        function reachsets = generate_ReachableSet(self,file) 
+
+            %loading the file
+            reachsets = loaders.RSLoader(file, do_caching=true, preload_data=false, preload_meta=false);
             collection_group = reachsets.getGroup(0);
             terminal_group = collection_group.getGroup(0);
             
-            vehrs = reachsets.getGroup(0).getGroup(0).getZonos;
-            search_set = reachsets.getSearchSet();
-            search_set = reachsets.getSearchSet().getZonos();
+            %extract vehrs
+            vehrs_ = reachsets.getGroup(0).getGroup(0).getZonos;
             zonos = terminal_group.getZonos();
-            z_matrix = zonos{1}.Z;
-            lat_speed = reachsets.getSearchSet().getZonos();
-            brake_idx1 = terminal_group.attributes.brake_index1;
-            brake_idx2 = terminal_group.attributes.brake_index2;
-            num_slicing = collection_group.num_groups;%slicing info 
-            
-            reach = FRS_loader_speed_change(vehrs,brake_idx1,brake_idx2 ,z_matrix);
+            total_num_states = length(zonos);
+            for i=1:total_num_states
+                z_matrix_ = zonos{i}.Z;
+                RobotState = rtd.entity.states.EntityState();
+                state = z_matrix_(1:6,:);
+                time = z_matrix_(20,:);
+                uuid = RobotState.uuid;
+                Robot_State = struct('uuid', uuid, 'state', state,'time',time);
+                
+                %store all values
+                self.z_matrix = z_matrix_;
+                self.vehrs = vehrs_;
+                self.robotState = Robot_State;
+                constraints_ = frs_create_constraints(self,vehrs_);
+                self.constraints = constraints_;
+                fprintf('%d number:',i);
+            end
+            disp('DONE')
+                      
         end
 
-        function self = FRS_loader_speed_change(vehrs,z_matrix)%pass all properties
-            arguments               
-              
-                vehrs
-                z_matrix 
-                
-            end
-           
-            Robot_State = z_matrix(1:6,:);
-            constraints = test2_FRS_Instance_speed_change(vehrs);
-            disp('FRS Instance completed')
-           
+
+        function constraints = frs_create_constraints(self,vehrs)
+
+            %create an instance to get the constraints
+            constraints = FRS_Instance_speed_change(vehrs);
+            self.constraints = constraints;
+            disp('FRS instance with constraints generated.')
+            
         end
     end
 
     %TO DO reachable set online
-   %  methods (Access=protected)
-   % 
-   %      function reachableSet = generateReachableSet(self, robotState, varargin)
-   %          reachableSet = getReachableSet(self,robotState);
-   %      end
-   % 
-   % 
-   % end
+    methods (Access=protected)
+
+        function reachableSet = generateReachableSet(self, robotState,varargin)%create an instance inside this to get constraints
+            %get reachable set
+            reachableSet = getReachableSet(self,robotState, varargin{:}, 'ignore_cache', true);
+            disp(reachableSet);
+
+            disp('Reachable Sets generated.');
+            exit();
+
+        end
+
+
+   end
 end
+
+
 
 
 
